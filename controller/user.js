9b7ccbcb6357838e28644d1256f5f7edf56775e0
unknown <okediran96@gmail.com>
2022-01-18 05:43:26 -0800

    update user

--- controller/user.js
+++ controller/user.js
@@ -3,10 +3,24 @@ const db = require("../models");
 module.exports = {
     update: async (req, res) => {
         const {language,skills,age,location,yearOfexperience,aboutme,availability,job} = req.body;
-        db.User.update({language,skills,age,location,yearOfexperience,aboutme,availability,job},
-            {
-                where: {id:req.user.id}
-            })
+        var userUpdate = {};
+        if(language) 
+            userUpdate["language"] = language
+        if(skills) 
+            userUpdate["skills"] = skills
+        if(location) 
+            userUpdate["location"] = location
+        if(age) 
+            userUpdate["age"] = age
+        if(yearOfexperience) 
+            userUpdate["yearOfexperience"] = yearOfexperience
+        if(aboutme) 
+            userUpdate["aboutme"] = aboutme
+        if(availability) 
+            userUpdate["availability"] = availability
+            
+        
+        db.User.update(userUpdate, {where: {id:req.user.id}})
         .then(()=>{
         res.status(200).json({message:'profile updated'})
         })
