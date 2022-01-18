ef339b55902ed661f7647f429ccce85010bf36b7
unknown <okediran96@gmail.com>
2022-01-18 01:22:23 -0800

    change datatypes in wallet

--- models/wallet.js
+++ models/wallet.js
@@ -19,7 +19,7 @@ module.exports= (sequelize,Datatypes) => {
             allowNull: false,
         },
         percentageRise: {
-            type: Datatypes.STRING(150),
+            type: Datatypes.INTEGER,
             allowNull: false,
         }
     });
